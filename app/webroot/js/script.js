f85fc02d673057bca53ddd711e56040596197056
vu <vunm@tmh-techlab.vn>
2016-05-18 17:31:29 +0700

    color

--- app/webroot/js/script.js
+++ app/webroot/js/script.js
@@ -4,6 +4,27 @@ $(document).ready(function () {
         .append($('<div id="action" class="action">'))
     );
 
+    $('#message').html('Enbi Tuyền không có duyên trong việc thi cử nên sau khi thi trượt đại học chàng trai này xin bố mẹ đi học retouch ảnh vào cuối năm 2007 sau đó đi làm luôn. \
+    Chia sẻ về lý do thích làm ảnh, chàng trai này cho biết: “Hồi nhỏ mình mê vẽ lắm, đến sau này có anh họ làm bên ảnh nên mình theo học”. \
+    Cũng như bao công việc khác, công việc này khiến chàng trai gặp khá nhiều khó khăn: “Công việc đòi hỏi làm khá nhiều trên máy tính nên đôi khi khá mệt mỏi, làm nhiều dễ bị căng thẳng khi gặp ảnh khó”. \
+    Nói thêm về thời gian làm ra một tác phẩm đẹp, chàng trai 8X này cho biết bình thường mất khoảng 1 hoặc 3h cho một tấm ảnh. Có khi ngồi làm từ sáng cho đến đêm nhất là vào mùa cưới. \
+    Vì tính chất công việc phải dán mặt vào máy tính hàng ngày nên chàng trai này thi thoảng cũng đôi khi phàn nàn, thế nhưng Tuyền cho biết anh chưa có ý định lập gia đình. \
+        Cùng ngắm một số tác phẩm tuyệt đẹp của Enbi Tuyền: \
+    Enbi Tuyền không có duyên trong việc thi cử nên sau khi thi trượt đại học chàng trai này xin bố mẹ đi học retouch ảnh vào cuối năm 2007 sau đó đi làm luôn. \
+    Chia sẻ về lý do thích làm ảnh, chàng trai này cho biết: “Hồi nhỏ mình mê vẽ lắm, đến sau này có anh họ làm bên ảnh nên mình theo học”. \
+    Cũng như bao công việc khác, công việc này khiến chàng trai gặp khá nhiều khó khăn: “Công việc đòi hỏi làm khá nhiều trên máy tính nên đôi khi khá mệt mỏi, làm nhiều dễ bị căng thẳng khi gặp ảnh khó”. \
+    Nói thêm về thời gian làm ra một tác phẩm đẹp, chàng trai 8X này cho biết bình thường mất khoảng 1 hoặc 3h cho một tấm ảnh. Có khi ngồi làm từ sáng cho đến đêm nhất là vào mùa cưới. \
+    Vì tính chất công việc phải dán mặt vào máy tính hàng ngày nên chàng trai này thi thoảng cũng đôi khi phàn nàn, thế nhưng Tuyền cho biết anh chưa có ý định lập gia đình. \
+        Cùng ngắm một số tác phẩm tuyệt đẹp của Enbi Tuyền: \
+        Vì tính chất công việc phải dán mặt vào máy tính hàng ngày nên chàng trai này thi thoảng cũng đôi khi phàn nàn, thế nhưng Tuyền cho biết anh chưa có ý định lập gia đình. \
+        Cùng ngắm một số tác phẩm tuyệt đẹp của Enbi Tuyền: \
+    Enbi Tuyền không có duyên trong việc thi cử nên sau khi thi trượt đại học chàng trai này xin bố mẹ đi học retouch ảnh vào cuối năm 2007 sau đó đi làm luôn. \
+    Chia sẻ về lý do thích làm ảnh, chàng trai này cho biết: “Hồi nhỏ mình mê vẽ lắm, đến sau này có anh họ làm bên ảnh nên mình theo học”. \
+    Cũng như bao công việc khác, công việc này khiến chàng trai gặp khá nhiều khó khăn: “Công việc đòi hỏi làm khá nhiều trên máy tính nên đôi khi khá mệt mỏi, làm nhiều dễ bị căng thẳng khi gặp ảnh khó”. \
+    Nói thêm về thời gian làm ra một tác phẩm đẹp, chàng trai 8X này cho biết bình thường mất khoảng 1 hoặc 3h cho một tấm ảnh. Có khi ngồi làm từ sáng cho đến đêm nhất là vào mùa cưới. \
+    Vì tính chất công việc phải dán mặt vào máy tính hàng ngày nên chàng trai này thi thoảng cũng đôi khi phàn nàn, thế nhưng Tuyền cho biết anh chưa có ý định lập gia đình. \
+        Cùng ngắm một số tác phẩm tuyệt đẹp của Enbi Tuyền: ');
+
     $('#action').append($('<div class="row">')
         .append($('<div class="col col-2 user">')
             .append($('<img src="/img/nobody.jpg">')))
